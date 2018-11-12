3a5d04b400ecb848c8a571ebfa8005de55ca079c
Lucas Barros <barrosdev8@gmail.com>
2018-11-12 20:56:10 -0200

    Challenge 10 concluded!

--- challenge-10/challenge-10.js
+++ challenge-10/challenge-10.js
@@ -1,3 +1,4 @@
+(function(){
 /*
 Crie uma IIFE que envolva todo esse arquivo (inclusive esse comentário),
 e faça a indentação correta.
@@ -9,10 +10,10 @@ deles seja "true", usando os Wrapper Objects como "conversores" nos valores
 das variáveis. Analise o que está sendo impresso no console para saber como
 resolver o problema corretamente.
 */
-var five = '5';
+var five = 5;
 console.log( five + ' é número?', typeof five === 'number' );
 
-var concat = 10 + 10;
+var concat = '10' + '10';
 console.log( '"' + concat + '" é uma string? E é igual a "1010"?', typeof concat === 'string' );
 
 /*
@@ -24,7 +25,34 @@ funcional, mas dessa vez, separando algumas responsabilidades.
 função receberá dois parâmetros e retornará a operação referente à sua
 propriedade, usando os valores passados por parâmetro.
 */
-// ?
+var operation = {
+    '+':
+        function( number1, number2 ){
+            return number1 + number2;
+    },
+
+    '-':
+        function( number1, number2 ){
+            return number1 - number2;
+    },
+
+    '*':
+        function( number1, number2 ){
+            return number1 * number2;
+    },
+
+    '/':
+        function( number1, number2 ){
+            return number1 / number2;
+    },
+
+    '%':
+        function( number1, number2 ){
+            return number1 % number2;
+    }
+
+}
+
 
 /*
 Crie uma função chamada `isOperatorValid`, que receberá um operador por
@@ -36,7 +64,12 @@ parâmetro a ela é válido, ou seja, se ele é igual a '+', '-', '*', '/' ou
 Caso contrário, "false".
 - O desafio é fazer o retorno sem usar "if" ou "switch".
 */
-// ?
+
+function isOperatorValid( operator ){
+
+    return !!operation[ operator ];
+
+}
 
 /*
 Agora vamos criar a calculadora.
@@ -50,7 +83,29 @@ parâmetros;
 operador passado para a função "calculator", e passando para esse método
 os dois parâmetros da função de retorno de "calculator".
 */
-// ?
+function calcutator( operator ){
+   
+    if( !isOperatorValid( operator )){
+        
+        return false;
+    
+    }
+    
+    return function( x , y ){
+        if( typeof x == 'number' && typeof y == 'number' ){
+
+            return operation[operator]( x, y );
+            
+                   
+        };
+
+        return false;
+    }
+
+    
+}
+
+
 
 /*
 Crie uma função chamada "showOperationMessage" que recebe três parâmetros:
@@ -59,7 +114,11 @@ deve ser a frase:
 'A operação [NUMBER1] [OPERATOR] [NUMBER2] =';
 Essa função mostrará a mensagem da operação que criaremos mais abaixo.
 */
-// ?
+function showOperationMessage( operator, number1, number2 ){
+
+    return 'A operação ' + number1 + ' ' + operator + ' ' + number2 +' = ';
+
+}
 
 /*
 Crie uma função chamada "showErrorMessage" que recebe um parâmetro: o
@@ -67,7 +126,11 @@ operador da operação cálculo, quando a operação não for válida.
 Essa função deverá retornar a frase:
 'Operação "[OPERATOR]" não permitida!'
 */
-// ?
+function showErrorMessage( operator ){
+
+    return 'Operação ' + operator + " não permitida!";
+
+}
 
 /*
 Nossa calculadora está pronta! Agora vamos testá-la:
@@ -75,7 +138,10 @@ PASSO 1:
 - Declare 3 variáveis: "number1" e "number2", iniciando com valor zero, e
 "operationSignal", sem valor por enquanto.
 */
-// ?
+var number1 = 0;
+var number2 = 0;
+var operationSignal;
+
 
 /*
 PASSO 2:
@@ -83,7 +149,9 @@ Atribua à variável operationSignal o operador de soma, e declare uma
 variável chamada "sum", que receba a função "calculator", passando por
 parâmetro a variável que recebeu o sinal da operação.
 */
-// ?
+operationSignal = '+';
+var sum = calcutator( operationSignal );
+
 
 /*
 PASSO 3:
@@ -97,17 +165,110 @@ parâmetros para o método "log" de "console":
 - O segundo, a função de soma, passando os dois operandos.
 - Se "sum" for "false", mostrar no console a mensagem de erro.
 */
-// ?
+
+if( sum ){
+
+    number1 = 5;
+    number2 = 58;
+    
+    console.log(showOperationMessage( operationSignal, number1, number2 ), sum( number1, number2 ));
+
+}else {
+
+    console.log(showErrorMessage( operationSignal ));
+
+}
 
 /*
 Repita desde o "PASSO 2" com as operações de subtração, multiplicação,
 divisão e resto. Crie variáveis com os nomes "subtraction",
 "multiplication", "division" e "mod".
 */
-// ?
+operationSignal = '-';
+var subtraction = calcutator( operationSignal );
+
+if ( subtraction ){
+    
+    number1 = 12;
+    number2 = 65;
+
+    console.log(showOperationMessage( operationSignal , number1, number2 ), subtraction( number1, number2 ));
+}
+else{
+    console.log(showErrorMessage( operationSignal ));
+}
+
+operationSignal = '*';
+
+var multiplication = calcutator( operationSignal );
+
+if( multiplication ){
+
+    number1 = 84;
+    number2 = 96;
+
+    console.log(showOperationMessage( operationSignal, number1, number2 ), multiplication( number1, number2 ));
+
+}
+else{
+    console.log(showErrorMessage( operationSignal ));
+
+}
+
+operationSignal = '/';
+
+var division = division = calcutator( operationSignal );
+
+if( division ){
+
+    number1 = 500;
+    number2 = 15;
+
+    console.log(showOperationMessage( operationSignal, number1, number2 ), division( number1, number2 ));
+
+}
+else{
+    console.log(showErrorMessage (operationSignal ));
+}
+
+
+operationSignal = '%';
+var mod = calcutator( operationSignal );
+
+if( mod ){
+
+    number1 = 40;
+    number2 = 3;
+
+    console.log(showOperationMessage( operationSignal, number1, number2 ), mod( number1, number2 ));
+
+}
+else {
+
+    console.log(showErrorMessage (operationSignal ));
+
+}
 
 /*
 Repita o PASSO 2 novamente, mas passando um operador inválido, para ver se
 a mensagem de erro será mostrada no console.
 */
-// ?
+operationSignal = '.';
+
+var dot = calcutator( operationSignal );
+
+if( dot ){
+
+    number1 = 5;
+    number2 = 85;
+
+    console.log(showOperationMessage( operationSignal, number1, number2 ), dot( number1, number2 ));
+
+}
+else{
+
+    console.log(showErrorMessage( operationSignal ));
+
+}
+
+}())
